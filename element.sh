798a27c709ce4f0bc3e24595fd34e8bc2d8f9661
sireeshaReddy9 <159791193+sireeshaReddy9@users.noreply.github.com>
2025-08-29 15:15:30 +0530

    Update element.sh

--- element.sh
+++ element.sh
@@ -31,4 +31,3 @@ else
   IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE_ID MASS MELT BOIL TYPE <<< "$ELEMENT"
   echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
 fi
-# Script for periodic table lookup
